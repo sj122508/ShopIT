29c98512dbe7c69aac49b412bc893c3f97cae97d
sj122508gh <jamesdviojan@gmail.com>
2021-06-10 18:34:36 +0800

    Get all Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,9 +11,14 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
-exports.getProducts = (req, res, next) => {
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: 'This route will show all products in database.'
+        count: products.length,
+        products
     })
 }
